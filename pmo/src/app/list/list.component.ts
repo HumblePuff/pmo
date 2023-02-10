1fd1f840bc431491e4cafbd639185a305e053ad5
Humble Puff <humblepufftime@gmail.com>
2023-02-10 03:46:01 -0300

    e

--- pmo/src/app/list/list.component.ts
+++ pmo/src/app/list/list.component.ts
@@ -1,30 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
   newTask: string;
   tasks: string[] = [];
-  showMyContainer: boolean = false;
-
+  showMyContainer: boolean = true;
   addTask(v) {
     this.tasks.push(v);
+    // this.showMyContainer = false;
     this.newTask = '';
   }
-
-
-
+  constructor(private host: ElementRef) {}
   removeTask(task: string) {
     this.tasks = this.tasks.filter(t => t !== task);
   }
-  
-
   ngOnInit(): void {
-      
   }
-
 }
